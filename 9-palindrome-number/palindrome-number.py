9aa17284f31975162201d226c780fac846be0048
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2025-07-01 22:18:58 -0600

    Time: 16 ms (30.22%) | Memory: 12.4 MB (51.08%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,23 +1,13 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-        
+class Solution(object):
+    def isPalindrome(self, x):
         number = str(x)
         n = len(number)
         mid = int(n/2)
-        n1 = number[0:mid]
-        if n % 2 != 0:
-            mid = mid+1
 
+        for i in range(mid):
+            if number[i] != number[(n-i-1)]:
+                
+                return False
         
-        n2 = ''
-        for i in range(n - 1, mid-1, -1):
-            n2 += number[i]
-    
-
-        if n1 == n2:
-            return True
-
-        return False
-            
+        return True
+        
