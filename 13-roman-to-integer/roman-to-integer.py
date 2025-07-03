0c250f435a0270a6aac996e132af28b0293e2a55
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2025-07-02 23:33:36 -0600

    Time: 3 ms (79.56%) | Memory: 17.7 MB (64.57%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -3,15 +3,13 @@ class Solution:
         roman = {
         "I":1,
         "V":5,
-        "X":10
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
         }
-        roman.update({"I":1})
-        roman.update({"V":5})
-        roman.update({"X":10})
-        roman.update({"L":50})
-        roman.update({"C":100})
-        roman.update({"D":500})
-        roman.update({"M":1000})
+      
         n = len(s)-1
         value = 0
         for i in range(n):
