25d22c075c243d1b5cd248141e0d7cc02b24454e
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2025-07-02 23:46:54 -0600

    Time: 3 ms (79.56%) | Memory: 18 MB (20.57%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman = {"Z":0,"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000,}
+        roman = {"Z":0,"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
       
         prev = 'Z'
         value = 0
@@ -11,7 +11,6 @@ class Solution:
                 value -= 2*prev_v
             prev = c
             value += current_v
-            
         return value
 
         
