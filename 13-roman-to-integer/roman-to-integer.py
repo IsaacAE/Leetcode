698a2c5c5c8c5be5b92c44ce6a5bf1d4138d54e5
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2025-07-02 23:29:25 -0600

    Time: 15 ms (5.34%) | Memory: 17.9 MB (20.57%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,25 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {}
+        roman.update({"I":1})
+        roman.update({"V":5})
+        roman.update({"X":10})
+        roman.update({"L":50})
+        roman.update({"C":100})
+        roman.update({"D":500})
+        roman.update({"M":1000})
+        n = len(s)-1
+        value = 0
+        for i in range(n):
+            actual = roman.get(s[i])
+            next = roman.get(s[i+1])
+            if actual < next:
+                value -= actual
+            else:
+                value +=actual
+            
+        value += roman.get(s[n])
+        
+        return value
+
+        
