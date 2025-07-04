9624327f426a94671027c577bb625720238c632a
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2025-07-04 02:04:05 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (32.32%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,12 +4,13 @@ class Solution:
         stack = []
 
         for paren in s:
-            if dict_paren.get(paren) != None:
-                if not stack or stack.pop() != dict_paren.get(paren):
+            current = dict_paren.get(paren)
+            if current != None:
+                if not stack or stack.pop() != current:
                     return False
             else:
                 stack.append(paren)
-                
+
         if len(stack) > 0:
             return False
         
