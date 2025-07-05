5dbe927b55eea4f801f4ee7855e4bd255158cf2b
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2025-07-05 13:25:39 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.15%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
+        i = 0
+        j = n-1
+        diff = 0
+
+        while i <= j:
+            if nums[i] == val:
+                
+                if nums[j] == val:
+                    j-=1
+                else:
+                    diff +=1
+                    nums[i] = nums[j]
+                    j-=1
+                    i+=1
+            else:
+                diff +=1 
+                i+=1  
+                
+            
+
+            
+
+       
+
+        return diff
+        
