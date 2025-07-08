1da21291ad1a9444217fc97971f41bf992b68ca7
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2025-07-07 23:14:37 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (8.56%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,23 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits)-1
+        k = 1
+        r = 0
+        digits[i]+=1
+        while i >= 0:
+            digits[i]+=r
+            r=0
+            if digits[i] >= 10:
+                r = digits[i] // 10
+                digits[i] = digits[i] % 10
+            i-=1
+
+        if r > 0:
+            head = [r]
+            head.extend(digits)
+            return head
+
+        return digits
+
+            
+        
