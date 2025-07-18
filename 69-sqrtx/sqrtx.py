896e7ce4fa77167e5291a0209c6ca01bd6bbb273
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2025-07-18 16:55:44 -0600

    Time: 3 ms (60.56%) | Memory: 18 MB (10.80%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,25 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+            
+        start = 0
+        end = x
+        search = int((start + end) / 2)
+        
+        while start <= end:
+            search = int((start + end) / 2)
+            res = search * search
+            if res == x:
+                return search
+            elif res > x:
+                end = search-1
+            else:
+                start = search+1
+                
+
+        if res > x:
+            return search-1
+        else:
+            return search
+            
