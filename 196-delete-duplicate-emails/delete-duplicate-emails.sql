97f5f9739f6d9b079a56a8a408ee2693e3397862
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2025-07-24 12:59:48 -0600

    Time: 226 ms (83.87%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-DELETE FROM Person where id not in (select min(id) from Person group by email having count(email) >= 1);
+DELETE FROM Person where id not in (select min(id) from Person group by email);
