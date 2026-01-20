b58bf4923f868c90d5cecbcb223c69b1409582cc
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2026-01-19 19:17:41 -0600

    Time: 213 ms (77.86%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query stateme
-select DISTINCT w2.id
-from Weather w2 inner join
-     Weather w1
-     ON w2.recordDate = w1.recordDate + INTERVAL '1 day'
-where w1.temperature < w2.temperature;
+select DISTINCT w1.id
+from Weather w1 inner join
+     Weather w2
+     ON w1.recordDate = w2.recordDate + INTERVAL '1 day'
+where w1.temperature > w2.temperature;
