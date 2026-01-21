d386197e893633a85c84692ce09cc5484dc527a9
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2026-01-20 23:18:57 -0600

    Time: 176 ms (77.01%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,6 +1,7 @@
 -- Write your PostgreSQL query statement below
 SELECT x, y, z, CASE 
-                WHEN x + y > z AND x+z>y AND z+y>x AND x+y+z>0 THEN 'Yes'
+                WHEN x < 1 OR z < 1 OR y < 1 THEN 'No'
+                WHEN x + y > z AND x + z > y AND z + y > x THEN 'Yes'
                 ELSE 'No'
                 END as triangle
                 FROM Triangle;
