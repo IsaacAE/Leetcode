6e78318cbdbb598e4386d7b101dcd3ec32e0c240
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2026-01-21 12:22:11 -0600

    Time: 205 ms (46.19%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,4 +1,4 @@
 -- Write your PostgreSQL query statement below
-SELECT max(salary) as SecondHighestSalary
-from Employee
-where salary < (SELECT Max(salary) from Employee)
+SELECT max(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT Max(salary) FROM Employee)
