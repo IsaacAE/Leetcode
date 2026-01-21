8b5322be2b25d22bb0e562cb1755fa86ea572027
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2026-01-21 12:36:02 -0600

    Time: 235 ms (26.91%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,16 +1,18 @@
 CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
 BEGIN
+
+   IF N <= 0 THEN
+        RETURN;
+   END IF;
+
   RETURN QUERY (
     SELECT (
       SELECT DISTINCT p.salary 
       FROM Employee p
       ORDER BY p.salary DESC 
-      -- Usamos OFFSET con un valor que nunca sea menor a 0
-      OFFSET GREATEST(N - 1, 0) 
+      OFFSET GREATEST(N - 1) 
       LIMIT 1
     )
-    -- El WHERE filtra si N es válido antes de que la consulta externa devuelva algo
-    WHERE N > 0
   );
 END;
 $$ LANGUAGE plpgsql;
