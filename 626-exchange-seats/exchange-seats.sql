e2381873f603e15ccd70ea759844f9351fb31368
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2026-02-06 12:43:46 -0600

    Time: 183 ms (53.55%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -2,10 +2,10 @@
 
 SELECT id, 
     (CASE 
-        WHEN id % 2 = 0 THEN LAG(student, 1) OVER (
+        WHEN id % 2 = 0 THEN LAG(student) OVER (
             ORDER BY
             id)
-        WHEN id % 2 = 1 AND id <> (SELECT MAX(id) FROM Seat) THEN LEAD(student, 1) OVER (
+        WHEN id % 2 = 1 AND id <> (SELECT MAX(id) FROM Seat) THEN LEAD(student) OVER (
             ORDER BY
             id)
         ELSE student
