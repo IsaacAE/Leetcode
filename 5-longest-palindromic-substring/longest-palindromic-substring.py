e4dea968ab515abdc8cc63b5ee8b1df5304376f1
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2026-06-20 20:17:50 -0600

    Time: 229 ms (83.41%) | Memory: 19.3 MB (37.11%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,54 +1,28 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        k = len(s)
+       
+        
+        def search_palindrome(i, j, sub:str):
+            left = i
+            right = j
+            while left >= 0 and right < len(sub) and sub[left] == sub[right]:
+                left-=1
+                right+=1
+
+            length = right-left
+            return [length, left+1, right-1]
+
         palindrome = s[:1]
-        while k >= 1:
-            sub = s[0:k]
-            if len(sub) % 2 == 1:
-                i = max(0, (len(sub) - 1) // 2)
-                j = min(len(sub) - 1, (len(sub) - 1) // 2)
-            else: 
-                i = max(0,len(sub)//2-1)
-                j = min(len(sub)-1,len(sub)//2)
-            
-            start = i
-            end = j
-            
-            
 
-            while i >= 0 and j < len(sub):
-                if sub[i] == sub[j]:
-                    if j - i >= len(palindrome):
-                        palindrome = sub[i:j+1]
-                else:
-                    i=-1
-                i-=1
-                j+=1
-            k-=1
-        k = 0
-        while k < len(s):
-            sub = s[k:len(s)]
-            if len(sub) % 2 == 1:
-                i = max(0, (len(sub) - 1) // 2)
-                j = min(len(sub) - 1, (len(sub) - 1) // 2)
-            else: 
-                i = max(0,len(sub)//2-1)
-                j = min(len(sub)-1,len(sub)//2)
-            
-            start = i
-            end = j
-            
-            
+        for i in range(0,len(s)):
+            odd = search_palindrome(i,i,s)
+            even = search_palindrome(i,i+1,s)
+            if odd[0]> even[0]:
+                res = odd
+            else:
+                res = even
 
-            while i >= 0 and j < len(sub):
-                if sub[i] == sub[j]:
-                    if j - i >= len(palindrome):
-                        palindrome = sub[i:j+1]
-                else:
-                    i=-1
-                i-=1
-                j+=1
-            k+=1
+            if res[0] > len(palindrome):
+                palindrome = s[res[1]:res[2]+1]
         return palindrome
-
         
