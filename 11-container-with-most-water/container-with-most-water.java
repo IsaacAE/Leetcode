f2def267e7cbb52df4e047b5caca0518f569c71b
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2026-06-21 17:30:54 -0600

    Time: 4 ms (94.30%) | Memory: 77.4 MB (48.22%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -3,24 +3,21 @@ class Solution {
         int i = 0;
         int j = height.length-1;
         int vol = Math.min(height[i],height[j]) * (j-i);
+        int testvol = 0;
         while (i < j){
             if (height[i] <= height[j]){
                 i++;
-               
-                    
-                    if (Math.min(height[i],height[j]) * (j-i) > vol){
-                   
-                    vol = Math.min(height[i],height[j]) * (j-i);
-                    }
                 
             }else{
                 j--;
-                
-                    if (Math.min(height[i],height[j]) * (j-i) > vol){
-                     vol = Math.min(height[i],height[j]) * (j-i);
-                    }
-                
 
+            }
+
+            
+            testvol = Math.min(height[i],height[j]) * (j-i);
+            if ( testvol > vol){
+            
+            vol = testvol;
             }
 
         }
