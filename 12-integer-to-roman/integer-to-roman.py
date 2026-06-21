c69e18561dcad553b48b48c3f4a4274a6bb9063e
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2026-06-20 23:10:45 -0600

    Time: 7 ms (40.61%) | Memory: 19.1 MB (90.69%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,34 +1,31 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        value_symbols = [
-            (1000, "M"),
-            (900,  "CM"),
-            (500,  "D"),
-            (400,  "CD"),
-            (100,  "C"),
-            (90,   "XC"),
-            (50,   "L"),
-            (40,   "XL"),
-            (10,   "X"),
-            (9,    "IX"),
-            (5,    "V"),
-            (4,    "IV"),
-            (1,    "I"),
-            (0,'')
-        ]
+        # 1. Definimos el diccionario de mayor a menor
+        value_symbols = {
+            1000: "M",
+            900:  "CM",
+            500:  "D",
+            400:  "CD",
+            100:  "C",
+            90:   "XC",
+            50:   "L",
+            40:   "XL",
+            10:   "X",
+            9:    "IX",
+            5:    "V",
+            4:    "IV",
+            1:    "I"
+        }
 
         res = []
 
-        for val, sym in value_symbols:
+        for val, sym in value_symbols.items():
+            
+            if num == 0:
+                break
+                
             while num >= val:
                 num -= val
                 res.append(sym)
-                if num == 0:
-                    break
-            
-           
+                
         return "".join(res)
-
-
-
-        
