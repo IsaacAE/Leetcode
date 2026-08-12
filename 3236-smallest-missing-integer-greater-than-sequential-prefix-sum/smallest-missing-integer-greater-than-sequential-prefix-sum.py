46b35b9bdf31e9ba19ab18ccbbf5693d7df511eb
IsaacAE <91040672+IsaacAE@users.noreply.github.com>
2026-08-11 18:53:15 -0600

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.58%) - LeetSync

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+
+    def missingInteger(self, nums):
+        prefix_sum = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1] + 1:
+                prefix_sum += nums[i]
+            else:
+                break
+
+        hash_table = {num: True for num in nums}
+
+        while prefix_sum in hash_table:
+            prefix_sum += 1
+
+        return prefix_sum
